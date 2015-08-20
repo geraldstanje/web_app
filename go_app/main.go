11ba7f5932f5613315c89628bbc8fda5143f3ee0
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-08-20 20:31:06 +0200

    added go app sources

---
+++ go_app/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "database/sql"
+  _ "github.com/lib/pq"
+  "fmt"
+)
+
+func main() {
+  fmt.Println("[database] Connecting to database...")
+  db, err = sql.Open("postgres", "postgres://admin:changeme@192.168.59.103/admin?sslmode=verify-full") //?sslmode=disable")
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Println("[database] Connected successfully.")
+}
