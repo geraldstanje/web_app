496f69547b880b391b2f65c9459ced0af04d69c6
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-08-20 20:37:12 +0200

    updated go app

---
+++ go_app/docker.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+build() {
+  docker build --no-cache -t outyet .
+  boot2docker up && $(boot2docker shellinit) 
+  boot2docker ip
+}
+
+run() {
+  # Run a docker
+  docker run -p 8080:8080 -t outyet
+}
+
+info() {
+  # docker ps
+  docker ps
+  # docker inspect hash
+}
+
+stopall() {
+  # docker stop hash
+  docker stop $(docker ps -a -q)
+}
+
+cleanall() {
+  # Remove all containers
+  docker rm $(docker ps -a -q)
+  # Romove all images
+  #docker rmi $(docker images -q)
+}
+
+case $1 in build|run|info|stopall|cleanall) "$1" ;; *) printf >&2 '%s: unknown command\n' "$1"; exit 1;; esac
--- go_app/main.go
+++ go_app/main.go
@@ -4,11 +4,12 @@ import (
   "database/sql"
   _ "github.com/lib/pq"
   "fmt"
+  "log"
 )
 
 func main() {
   fmt.Println("[database] Connecting to database...")
-  db, err = sql.Open("postgres", "postgres://admin:changeme@192.168.59.103/admin?sslmode=verify-full") //?sslmode=disable")
+  db, err := sql.Open("postgres", "postgres://admin:changeme@192.168.59.103/admin?sslmode=verify-full") //?sslmode=disable")
   if err != nil {
     log.Fatal(err)
   }
