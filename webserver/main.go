5452db2e3f13694fb690c0ecff700890f27ec8ba
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-08-21 20:53:48 +0200

    added simple webserver

--- webserver/main.go
+++ webserver/main.go
@@ -1,21 +1,44 @@
 package main
 
 import (
+  "io"
+  "net/http"
   "database/sql"
   _ "github.com/lib/pq"
   "fmt"
-  "log"
 )
 
+type struct Logger {
+  msg string
+}
+
+func NewLogger() *Logger {
+  return &Logger{}
+}
+
+func (l *Logger) EmitLine(line string) {
+  l.writer.WriteString(line)
+  l.writer.WriteString("\n")
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request, msg string) {
+  io.WriteString(w, msg)
+}
+
 func main() {
-  fmt.Println("[database] Connecting to database...")
+  l := NewLogger()
+
+  l.EmitLine("[database] Connecting to database...")
   db, err := sql.Open("postgres", "postgres://admin:changeme@192.168.59.103:5432/admin?sslmode=disable") //?sslmode=verify-full")
   if err != nil {
-    log.Fatal(err)
+    l.EmitLine(err.Error())
   }
   err = db.Ping()
   if err != nil {
-    log.Fatal(err)
-  }
-  fmt.Println("[database] Connected successfully.")
+    l.EmitLine(err.Error())
+  } 
+  l.EmitLine("[database] Connected successfully.")
+
+  http.HandleFunc("/", makeHandler(homeHandler))
+  http.ListenAndServe(":8000", nil)
 }
