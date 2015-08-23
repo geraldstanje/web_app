71fb693b3d0d9530e560d481629237d523bf392e
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-08-23 18:32:49 +0200

    fixed compile error

--- webserver/authentication/authentication.go
+++ webserver/authentication/authentication.go
@@ -5,7 +5,7 @@ import (
   s "github.com/geraldstanje/web_app/webserver/session"
 )
 
-func LoginHandler(response http.ResponseWriter, request *http.Request) {
+func Login(response http.ResponseWriter, request *http.Request) {
   name := request.FormValue("name")
   pass := request.FormValue("password")
   redirectTarget := "/"
@@ -17,7 +17,7 @@ func LoginHandler(response http.ResponseWriter, request *http.Request) {
   http.Redirect(response, request, redirectTarget, 302)
 }
 
-func LogoutHandler(response http.ResponseWriter, request *http.Request) {
+func Logout(response http.ResponseWriter, request *http.Request) {
   s.ClearSession(response)
   http.Redirect(response, request, "/", 302)
 }
