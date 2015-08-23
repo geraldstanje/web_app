b6dc14ea77e34c9bfd6ab80e32bec8a2387045c1
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-08-23 19:38:05 +0200

    updated db init script

--- postgres/init-scripts/create_db.sql
+++ postgres/init-scripts/create_db.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS account (
   email VARCHAR(32) NOT NULL PRIMARY KEY,
   username VARCHAR(32) NOT NULL UNIQUE,
-  password CHAR(32) NOT NULL
+  password VARCHAR(32) NOT NULL
 );
 
 INSERT INTO account VALUES('Christiano.Ronaldo@gmail.com', 'Christiano' , 'Ronaldo');
