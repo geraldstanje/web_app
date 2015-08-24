622ba3eb75eca9869157b1369f303958f6191888
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-08-24 19:39:33 +0200

    added json

--- webserver/authentication/authentication.go
+++ webserver/authentication/authentication.go
@@ -1,24 +1,44 @@
 package authentication
 
 import (
+  "encoding/json"
+  "log"
 	d "github.com/geraldstanje/web_app/webserver/db"
 	s "github.com/geraldstanje/web_app/webserver/session"
 	"net/http"
 )
 
+type Message struct {
+    Succeed bool `json:"succeed"`
+    Error string `json:"error"`
+    Redirect string `json:"redirect"`
+}
+
 func Login(w http.ResponseWriter, r *http.Request) {
 	user := r.FormValue("email")
 	pass := r.FormValue("password")
-	redirectTarget := "/"
+  succeed := false
+  error := ""
+  redirectTarget := "/"
 
 	if user != "" && pass != "" && d.IsValidLogin(user, pass) {
 		s.SetSession(user, w)
-		redirectTarget = "/musicalbums"
-    http.Redirect(w, r, redirectTarget, 302)
-	} else {
-    str := "Invalid username or password"
-    w.Write([]byte(str))
+    succeed = true
+    error = "Invalid user or password"
+    redirectTarget = "/musicalbums"
+    //http.Redirect(w, r, redirectTarget, 302)
+	} //else {
+    //str := "Invalid username or password"
+    //w.Write([]byte(str))
 	  //http.Redirect(w, r, redirectTarget, 302)
+  //}
+
+  m := Message{succeed, error, redirectTarget}
+
+  //w.Header().Set("Content-Type", "application/json")
+  //json.NewEncoder(w).Encode(m)
+  if err := json.NewEncoder(w).Encode(m); err != nil {
+    log.Println("[webserver] " + err.Error())
   }
 }
 
