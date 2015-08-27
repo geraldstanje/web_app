d242258ce53dedfd565fb1b032723aec737768a3
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-08-27 23:26:30 +0200

    updated docker script

--- docker.sh
+++ docker.sh
@@ -12,8 +12,13 @@ build() {
 }
 
 test() {
+  # start docker postgresql_db
   docker run -d -p 5432:5432 -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=changeme --name postgresql_db -v "/var/lib/postgresql/data:/var/lib/postgresql/data" outyet1
+  # run test
   docker run -ti --rm --entrypoint /bin/bash outyet2 scripts/unit_test.sh
+  # stop docker postgresql_db
+  docker kill postgresql_db
+  docker rm postgresql_db
 }
 
 run() {
@@ -22,9 +27,7 @@ run() {
   # the difference between docker run -v /host/path:/container/path and docker run -v /container/path is, 
   # that in the first case you provide a directory to mount in, in the 2nd case, docker creates a directory 
   # in /var/lib/docker and uses that as the host path
-  cd postgres;
   docker run -d -p 5432:5432 -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=changeme --name postgresql_db -v "/var/lib/postgresql/data:/var/lib/postgresql/data" outyet1
-  cd ../webserver;
   docker run -d -p 8080:8080 --name webserver -v "/var/volume1:/go/src/github.com/geraldstanje/web_app/webserver/files" outyet2
 }
 
@@ -41,30 +44,13 @@ logs() {
 
 stop() {
   # docker stop with name
-  cd postgres;
-  docker stop postgresql_db
-  cd ../webserver;
-  docker stop webserver
-}
-
-stopall() {
-  # docker stop hash
-  docker stop $(docker ps -a -q)
-}
-
-clean() {
-  # Remove container with name
-  cd postgres;
+  # stop docker postgresql_db
+  docker kill postgresql_db
   docker rm postgresql_db
-  cd ../webserver;
-  docker stop webserver
-}
 
-cleanall() {
-  # Remove all containers
-  docker rm $(docker ps -a -q)
-  # Romove all images
-  #docker rmi $(docker images -q)
+  # stop docker webserver
+  docker kill webserver
+  docker rm webserver
 }
 
 # Delete all containers
@@ -73,4 +59,4 @@ cleanall() {
 # Delete all images
 # docker rmi $(docker images -q)
 
-case $1 in build|test|run|info|logs|stop|stopall|clean|cleanall) "$1" ;; *) printf >&2 '%s: unknown command\n' "$1"; exit 1;; esac
+case $1 in build|test|run|info|logs|stop) "$1" ;; *) printf >&2 '%s: unknown command\n' "$1"; exit 1;; esac
