85cb4400df24a0699be952a3bd1a823f8bb86282
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-08-27 22:34:40 +0200

    extended test

--- webserver/authentication/authentication_test.go
+++ webserver/authentication/authentication_test.go
@@ -13,8 +13,12 @@ func TestRegister(t *testing.T) {
   v := url.Values{}
   v.Add("email", "test@gmail.com")
   v.Add("password", "root")  
+  
   req, _ := http.NewRequest("POST", "", bytes.NewBufferString(v.Encode()))
   w := httptest.NewRecorder()
   Register(w, req)
-  fmt.Println(w)
+
+  if w.Code != http.StatusOK {
+    t.Errorf("Home page didn't return %v", http.StatusOK)
+  }
 }
