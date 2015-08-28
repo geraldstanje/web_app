3d3b5919e66130d2b6761d6110e5b3e953d2b75b
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-08-28 18:44:03 +0200

    changed authentication test

--- webserver/authentication/authentication_test.go
+++ webserver/authentication/authentication_test.go
@@ -1,45 +1,45 @@
 package authentication
 
-import(
-    "net/http"
-    "net/http/httptest"
-    "testing"
-    "net/url"
-    "strings"
-    d "github.com/geraldstanje/web_app/webserver/db"
-    "json"
+import (
+	"encoding/json"
+	d "github.com/geraldstanje/web_app/webserver/db"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
 )
 
 type Message struct {
-  Succeed  bool   `json:"succeed"`
-  Info     string `json:"info"`
-  Redirect string `json:"redirect"`
+	Succeed  bool   `json:"succeed"`
+	Info     string `json:"info"`
+	Redirect string `json:"redirect"`
 }
 
 func TestRegister(t *testing.T) {
-  t.Log("Run TestRegister...")
+	t.Log("Run TestRegister...")
 
-  _ = d.RemoveUser("test@gmail.com")
+	_ = d.RemoveUser("test@gmail.com")
 
-  v := url.Values{}
-  v.Add("email", "test@gmail.com")
-  v.Add("password", "root")  
-  
-  req, _ := http.NewRequest("POST", "", strings.NewReader(v.Encode()))
-  req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-  w := httptest.NewRecorder()
-  Register(w, req)
+	v := url.Values{}
+	v.Add("email", "test@gmail.com")
+	v.Add("password", "root")
 
-  if w.Code != http.StatusOK {
-    t.Errorf("Home page didn't return %v", http.StatusOK)
-  }
+	req, _ := http.NewRequest("POST", "", strings.NewReader(v.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	w := httptest.NewRecorder()
+	Register(w, req)
 
-  var m Message
-  if err := json.NewDecoder(req.Body).Decode(&m); err != nil {
-    t.Errorf(err.Error())
-  }
+	if w.Code != http.StatusOK {
+		t.Errorf("Home page didn't return %v", http.StatusOK)
+	}
 
-  t.log("m: ", m)
+	var m Message
+	if err := json.NewDecoder(req.Body).Decode(&m); err != nil {
+		t.Errorf(err.Error())
+	}
 
-  t.Log("Run TestRegister...successful")
+	t.log("m: ", m)
+
+	t.Log("Run TestRegister...successful")
 }
