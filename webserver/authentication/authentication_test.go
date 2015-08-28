636e16d3d40439926c4f0ae9c5aa2854e83456d7
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-08-28 19:01:08 +0200

    changed authentication test

--- webserver/authentication/authentication_test.go
+++ webserver/authentication/authentication_test.go
@@ -10,28 +10,58 @@ import (
 	"testing"
 )
 
+func FakeRegister(user string, pass string) {
+  _ = d.RemoveUser(user)
+
+  v := url.Values{}
+  v.Add("email", user)
+  v.Add("password", pass)
+
+  req, _ := http.NewRequest("POST", "", strings.NewReader(v.Encode()))
+  req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+  w := httptest.NewRecorder()
+  Register(w, req)
+
+  if w.Code != http.StatusOK {
+    t.Errorf("Home page didn't return %v", http.StatusOK)
+  }
+
+  var m Message
+  if err := json.NewDecoder(w.Body).Decode(&m); err != nil {
+    t.Errorf(err.Error())
+  }
+
+  if m.Succeed != true {
+    t.Errorf("Wrong json response")
+  }
+}
+
 func TestRegister(t *testing.T) {
-	_ = d.RemoveUser("test@gmail.com")
+	FakeRegister("test@gmail.com", "root")
+}
 
-	v := url.Values{}
-	v.Add("email", "test@gmail.com")
-	v.Add("password", "root")
+func TestLogin(t *testing.T) {
+  FakeRegister("test@gmail.com", "root")
 
-	req, _ := http.NewRequest("POST", "", strings.NewReader(v.Encode()))
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	w := httptest.NewRecorder()
-	Register(w, req)
+  v := url.Values{}
+  v.Add("email", "test@gmail.com")
+  v.Add("password", "root")
 
-	if w.Code != http.StatusOK {
-		t.Errorf("Home page didn't return %v", http.StatusOK)
-	}
+  req, _ := http.NewRequest("POST", "", strings.NewReader(v.Encode()))
+  req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+  w := httptest.NewRecorder()
+  Login(w, req)
 
-	var m Message
-	if err := json.NewDecoder(w.Body).Decode(&m); err != nil {
-		t.Errorf(err.Error())
-	}
+  if w.Code != http.StatusOK {
+    t.Errorf("Home page didn't return %v", http.StatusOK)
+  }
 
-	if m.Succeed != true {
-		t.Errorf("Wrong json response")
-	}
+  var m Message
+  if err := json.NewDecoder(w.Body).Decode(&m); err != nil {
+    t.Errorf(err.Error())
+  }
+
+  if m.Succeed != true || m.Redirect != "/musicalbums" {
+    t.Errorf("Wrong json response")
+  }
 }
