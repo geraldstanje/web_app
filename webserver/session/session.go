28ffd4e0e7de6e456685c0d9f2d8933698fc1efa
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-08-30 01:07:59 +0200

    changed session

--- webserver/session/session.go
+++ webserver/session/session.go
@@ -7,47 +7,38 @@ import (
   "time"
 )
 
-var cookieHandler = securecookie.New(securecookie.GenerateRandomKey(64), securecookie.GenerateRandomKey(32))
+var cookieStore = securecookie.New(securecookie.GenerateRandomKey(64), securecookie.GenerateRandomKey(32))
 
 func SetSession(userName string, w http.ResponseWriter) {
-  t := strconv.FormatInt(time.Now().UnixNano(), 10)
   value := map[string]string{
     "user": userName,
-    "time": t,
   }
-  if encoded, err := cookieHandler.Encode("session", value); err == nil {
-    cookie := &http.Cookie{Name: "session", Value: encoded, Path: "/"}
+  if encoded, err := cookieStore.Encode("session", value); err == nil {
+    cookie := &http.Cookie{
+      Expires: time.Now().UTC().AddDate(1, 0, 0),
+      Name:  "session",
+      Value: encoded,
+      Path:  "/",
+    }
     http.SetCookie(w, cookie)
   }
 }
 
 func ClearSession(w http.ResponseWriter) {
-  t := strconv.FormatInt(time.Now().UnixNano(), 10)
-  value := map[string]string{
-    "time": t,
+  cookie := &http.Cookie{
+    MaxAge: -1,
+    Name:   "session",
+    Path:   "/",
   }
-  if encoded, err := cookieHandler.Encode("session", value); err == nil {
-    cookie := &http.Cookie{Name: "session", Value: encoded, MaxAge: -1, Expires: time.Unix(1, 0)}
-    http.SetCookie(w, cookie)
-  }
+  http.SetCookie(w, cookie)
 }
 
 func GetSessionUser(r *http.Request) (userName string, err error) {
   if cookie, err := r.Cookie("session"); err == nil {
     cookieValue := make(map[string]string)
-    if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
+    if err = cookieStore.Decode("session", cookie.Value, &cookieValue); err == nil {
       userName = cookieValue["user"]
     }
   }
   return userName, err
-}
-
-func GetSessionTime(r *http.Request) (t string, err error) {
-  if cookie, err := r.Cookie("session"); err == nil {
-    cookieValue := make(map[string]string)
-    if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
-      t = cookieValue["time"]
-    }
-  }
-  return t, err
 }
