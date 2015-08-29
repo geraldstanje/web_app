b5e4d3e69b9dfbece5ff6f865eb9dbbd22f3040d
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-08-29 12:32:08 +0200

    extended db test

--- webserver/db/db_test.go
+++ webserver/db/db_test.go
@@ -7,11 +7,23 @@ import (
 func TestUserAdd(t *testing.T) {
   res := AddUser("Max.Musterman@gmail.com", "admin")
   if res != true {
-    t.Errorf("Add user failed")
+    t.Errorf("AddUser failed")
   }
 
   res = AddUser("Max.Musterman@gmail.com", "admin")
   if res != false {
-    t.Errorf("Add user failed")
+    t.Errorf("AddUser failed")
+  }
+}
+
+func TestCheckUserLogin(t *testing.T) {
+  res := AddUser("Kevin_De_Bruyne@gmail.com", "admin")
+  if res != true {
+    t.Errorf("AddUser failed")
+  }
+
+  res = CheckUserLogin("Max.Musterman@gmail.com", "admin")
+  if res != true {
+    t.Errorf("CheckUserLogin failed")
   }
 }
