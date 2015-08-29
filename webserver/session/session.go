877f14d358ae3ea5c05b4e3c44e2691fb19dbee2
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-08-29 23:39:54 +0200

    fixed sessions

--- webserver/session/session.go
+++ webserver/session/session.go
@@ -3,51 +3,47 @@ package session
 import (
 	"github.com/gorilla/securecookie"
 	"net/http"
-	"strconv"
-	"time"
+  "log"
+  "errors"
 )
 
 var cookieHandler = securecookie.New(securecookie.GenerateRandomKey(64), securecookie.GenerateRandomKey(32))
 
 func SetSession(userName string, w http.ResponseWriter) {
-	t := strconv.FormatInt(time.Now().UnixNano(), 10)
 	value := map[string]string{
 		"user": userName,
-		"time": t,
 	}
 	if encoded, err := cookieHandler.Encode("session", value); err == nil {
-		cookie := &http.Cookie{Name: "session", Value: encoded, Path: "/"}
+		cookie := &http.Cookie{
+			Name:  "session",
+			Value: encoded,
+			Path:  "/",
+		}
 		http.SetCookie(w, cookie)
 	}
 }
 
 func ClearSession(w http.ResponseWriter) {
-	t := strconv.FormatInt(time.Now().UnixNano(), 10)
-	value := map[string]string{
-		"time": t,
+	cookie := &http.Cookie{
+		Name:   "session",
+		Value:  "",
+		Path:   "/",
+		MaxAge: -1,
 	}
-	if encoded, err := cookieHandler.Encode("session", value); err == nil {
-		cookie := &http.Cookie{Name: "session", Value: encoded, MaxAge: -1, Expires: time.Unix(1, 0)}
-		http.SetCookie(w, cookie)
-	}
+
+	http.SetCookie(w, cookie)
 }
 
 func GetSessionUser(r *http.Request) (userName string, err error) {
 	if cookie, err := r.Cookie("session"); err == nil {
 		cookieValue := make(map[string]string)
 		if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
-			userName = cookieValue["user"]
+      if cookie.MaxAge == -1 {
+        err = errors.New("session expired")
+      } else {
+			  userName = cookieValue["user"]
+      }
 		}
 	}
 	return userName, err
-}
-
-func GetSessionTime(r *http.Request) (t string, err error) {
-	if cookie, err := r.Cookie("session"); err == nil {
-		cookieValue := make(map[string]string)
-		if err = cookieHandler.Decode("session", cookie.Value, &cookieValue); err == nil {
-			t = cookieValue["time"]
-		}
-	}
-	return t, err
 }
