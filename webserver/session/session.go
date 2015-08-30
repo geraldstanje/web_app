aaa3c88a514796afa8ccf13406d41c515b78668a
Gerald Stanje <Gerald.Stanje@gmail.com>
2015-08-30 12:32:43 +0200

    formatted code

--- webserver/session/session.go
+++ webserver/session/session.go
@@ -1,44 +1,44 @@
 package session
 
 import (
-  "github.com/gorilla/securecookie"
-  "net/http"
-  "strconv"
-  "time"
+	"github.com/gorilla/securecookie"
+	"net/http"
+	"strconv"
+	"time"
 )
 
 var cookieStore = securecookie.New(securecookie.GenerateRandomKey(64), securecookie.GenerateRandomKey(32))
 
 func SetSession(userName string, w http.ResponseWriter) {
-  value := map[string]string{
-    "user": userName,
-  }
-  if encoded, err := cookieStore.Encode("session", value); err == nil {
-    cookie := &http.Cookie{
-      Expires: time.Now().UTC().AddDate(1, 0, 0),
-      Name:  "session",
-      Value: encoded,
-      Path:  "/",
-    }
-    http.SetCookie(w, cookie)
-  }
+	value := map[string]string{
+		"user": userName,
+	}
+	if encoded, err := cookieStore.Encode("session", value); err == nil {
+		cookie := &http.Cookie{
+			Expires: time.Now().UTC().AddDate(1, 0, 0),
+			Name:    "session",
+			Value:   encoded,
+			Path:    "/",
+		}
+		http.SetCookie(w, cookie)
+	}
 }
 
 func ClearSession(w http.ResponseWriter) {
-  cookie := &http.Cookie{
-    MaxAge: -1,
-    Name:   "session",
-    Path:   "/",
-  }
-  http.SetCookie(w, cookie)
+	cookie := &http.Cookie{
+		MaxAge: -1,
+		Name:   "session",
+		Path:   "/",
+	}
+	http.SetCookie(w, cookie)
 }
 
 func GetSessionUser(r *http.Request) (userName string, err error) {
-  if cookie, err := r.Cookie("session"); err == nil {
-    cookieValue := make(map[string]string)
-    if err = cookieStore.Decode("session", cookie.Value, &cookieValue); err == nil {
-      userName = cookieValue["user"]
-    }
-  }
-  return userName, err
+	if cookie, err := r.Cookie("session"); err == nil {
+		cookieValue := make(map[string]string)
+		if err = cookieStore.Decode("session", cookie.Value, &cookieValue); err == nil {
+			userName = cookieValue["user"]
+		}
+	}
+	return userName, err
 }
